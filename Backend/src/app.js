b045fcd696c7b607b486d73d99ce57d8120e8ebb
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-02-05 19:44:35 +0530

    mini changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -81,7 +81,7 @@ app.patch('/api/notes/:id', async (req,res)=>{
 
 
 app.use("*name", (req, res) => {
-res.sendFile(path.join(__dirname, "../public/index.html"))
+res.sendFile(path.join(__dirname,"..","/public/index.html"))
 });
 
 
