b28bb347ba8ac18363e2e7adf5af241a23098d16
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-02-09 09:09:18 +0530

    path changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const path = require("path")
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static("/public"))
+app.use(express.static("./public"))
 
 /*
 --POST /api/notes
